8d4061326323b61f9dc2928ac9b47fb2e191cc9f
Samson Mubeya <rasspartah@gmail.com>
2024-04-28 14:01:44 +0300

    did my assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Have you ever wondered if how somepeople do business without ever contacting customers but customers contacting them. Well a website and a greate social media presence is one way to do it. Im her to do that for you.</p>
+    <img src={image} alt="I made this"/>
+  </div>;
 }
 
 export default About;
